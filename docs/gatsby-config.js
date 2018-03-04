f9185c18c1188e32419402fd32492abc2f6e2383
Nathan Gaberel <nathang@theodo.co.uk>
2018-03-04 16:32:19 +0000

    Install styled-components.

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-react-next',
+    'gatsby-plugin-styled-components',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
